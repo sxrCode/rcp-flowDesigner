7c74cd9ed765e9bd072b3c28ad386d1de623d9c4
sunxiran <maricle01@163.com>
2017-06-22 11:40:18 +0800

    修改登录按钮

--- src/test2/view/LoginView.java
+++ src/test2/view/LoginView.java
@@ -3,9 +3,13 @@ package test2.view;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -26,7 +30,7 @@ public class LoginView {
 	private Composite loginPanel;
 	private Text fTextUsername;
 	private Text fTextPassword;
-	private Button fButtonOK;
+	private Composite buttonLogin;
 	private Button fButtonCancel;
 
 	private boolean fAuthenticated = false;
@@ -64,14 +68,14 @@ public class LoginView {
 	}
 
 	private void addButtonEvent() {
-		fButtonOK.addSelectionListener(new SelectionAdapter() {
-
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("say yes!");
-				fAuthenticated = true;
-			}
-		});
+		if (buttonLogin != null) {
+			buttonLogin.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseDown(MouseEvent e) {
+					fAuthenticated = true;
+				}
+			});
+		}
 
 		fButtonCancel.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -120,7 +124,7 @@ public class LoginView {
 		loginPanelLayout.marginWidth = 0;
 		loginPanel.setBounds(loginShell.getBounds());
 		loginPanelBounds = loginPanel.getBounds();
-		loginPanel.setBackground(new Color(loginShell.getDisplay(), 0, 0, 0));
+		loginPanel.setBackground(new Color(loginShell.getDisplay(), 235, 242, 249));
 
 		createUICompositeBanner();
 		createUILoginContent();
@@ -149,7 +153,7 @@ public class LoginView {
 
 	private void createUILoginContent() {
 		Composite content = new Composite(loginPanel, SWT.NONE);
-		content.setBackground(new Color(loginShell.getDisplay(), 10, 200, 30));
+		content.setBackground(new Color(loginShell.getDisplay(), 235, 242, 249));
 		content.setSize(loginPanelBounds.width, (int) (loginPanelBounds.height * 0.3));
 		GridData contentLayoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		// contentLayoutData.minimumHeight = (int) (loginPanelBounds.height *
@@ -163,12 +167,12 @@ public class LoginView {
 		System.out.println("content: " + content.getBounds().toString());
 
 		createUIBlank(content, 0.1);
-		createUILabelUserName(content, 0.4);
-		createUITextUserName(content, 0.3);
+		createUILabelUserName(content, 0.3);
+		createUITextUserName(content, 0.4);
 		createUIBlank(content, 0.1);
 
 		createUIBlank(content, 0.1);
-		createUILabelPassword(content, 0.4);
+		createUILabelPassword(content, 0.3);
 		createUITextPassword(content, 0.4);
 		createUIBlank(content, 0.1);
 
@@ -211,8 +215,9 @@ public class LoginView {
 		composite.setLayoutData(data);
 		fTextUsername = new Text(composite, SWT.BORDER | SWT.LEAD);
 		RowData rowData = new RowData();
-		rowData.width = data.minimumWidth;
+		rowData.width = (int) (data.minimumWidth * 0.8);
 		fTextUsername.setLayoutData(rowData);
+		fTextUsername.setTextLimit(10);
 		composite.setLayout(new RowLayout());
 		composite.setBackground(new Color(loginShell.getDisplay(), 255, 255, 240));
 	}
@@ -221,11 +226,21 @@ public class LoginView {
 		Composite composite = new Composite(parent, SWT.NONE);
 		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
 		data.minimumWidth = (int) (parent.getBounds().width * widthPre);
+
 		Label label = new Label(composite, SWT.CENTER);
-		label.setText("&Password:"); //$NON-NLS-1$
+		label.setAlignment(SWT.CENTER);
+		label.setText("Password:"); //$NON-NLS-1$
+		RowData rowData = new RowData((int) (data.minimumWidth * 0.8), 30);
+		label.setLayoutData(rowData);
+		label.setBackground(new Color(loginShell.getDisplay(), 220, 220, 220));
+
 		// Configure layout data
 		composite.setLayoutData(data);
-		composite.setBackground(new Color(loginShell.getDisplay(), 0, 229, 238));
+		composite.setBackground(new Color(loginShell.getDisplay(), 106, 90, 205));
+		RowLayout layout = new RowLayout(SWT.VERTICAL);
+		layout.center = true;
+		layout.wrap = false;
+		composite.setLayout(layout);
 	}
 
 	private void createUITextPassword(Composite parent, double widthPre) {
@@ -235,14 +250,18 @@ public class LoginView {
 		data.minimumWidth = (int) (parent.getBounds().width * widthPre);
 		composite.setLayoutData(data);
 		fTextPassword = new Text(composite, SWT.BORDER | SWT.LEAD);
-		composite.setBackground(new Color(loginShell.getDisplay(), 255, 218, 185));
+		RowData rowData = new RowData();
+		rowData.width = (int) (data.minimumWidth * 0.8);
+		fTextPassword.setLayoutData(rowData);
+		fTextPassword.setTextLimit(10);
+		composite.setLayout(new RowLayout());
+		composite.setBackground(new Color(loginShell.getDisplay(), 235, 242, 249));
 	}
 
 	private void createUIButton() {
 		Composite buttonContent = new Composite(loginPanel, SWT.NONE);
-		buttonContent.setBackground(new Color(loginShell.getDisplay(), 150, 20, 40));
-		// buttonContent.setSize(loginPanelBounds.width, (int)
-		// (loginPanelBounds.height * 0.2));
+		buttonContent.setBackground(new Color(loginShell.getDisplay(), 235, 242, 249));
+		buttonContent.setSize(loginPanelBounds.width, (int) (loginPanelBounds.height * 0.2));
 		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		gridData.minimumHeight = (int) (loginPanelBounds.height * 0.2);
 		buttonContent.setLayoutData(gridData);
@@ -255,24 +274,25 @@ public class LoginView {
 	}
 
 	private void createUIButtonOK(Composite parent) {
-		// Create the button
-		fButtonOK = new Button(parent, SWT.PUSH | SWT.CENTER);
-		fButtonOK.setText("登陆"); //$NON-NLS-1$
-		// Configure layout data
-		GridData data = new GridData(SWT.NONE, SWT.NONE, true, false);
-		// data.widthHint = F_BUTTON_WIDTH_HINT;
-		// data.verticalIndent = 10;
-		fButtonOK.setLayoutData(data);
+		buttonLogin = new Composite(parent, SWT.NONE);
+		GridData data = new GridData(GridData.CENTER, GridData.CENTER, true, true);
+		data.widthHint = (int) (parent.getBounds().width * 0.326);
+		data.heightHint = (int) (parent.getBounds().height * 0.55);
+		buttonLogin.setLayoutData(data);
+		buttonLogin.setSize((int) (parent.getBounds().width * 0.326), (int) (parent.getBounds().height * 0.55));
+		buttonLogin.setBackground(new Color(loginShell.getDisplay(), 173, 255, 47));
+		System.out.println("buttonok: " + buttonLogin.getBounds());
+		Image image = ImageHelper.getImage(ImageHelper.LOGIN_IMG);
+		ImageData imageData = image.getImageData().scaledTo(buttonLogin.getBounds().width,
+				buttonLogin.getBounds().height);
+		buttonLogin.setBackgroundImage(new Image(loginShell.getDisplay(), imageData));
 	}
 
 	private void createUIButtonCancel(Composite parent) {
-		// Create the button
 		fButtonCancel = new Button(parent, SWT.PUSH | SWT.CENTER);
 		fButtonCancel.setText("取消"); //$NON-NLS-1$
-		// Configure layout data
-		GridData data = new GridData(SWT.NONE, SWT.NONE, true, false);
-		// data.widthHint = F_BUTTON_WIDTH_HINT;
-		// data.verticalIndent = 10;
+		GridData data = new GridData(GridData.CENTER, GridData.CENTER, true, true);
+
 		fButtonCancel.setLayoutData(data);
 	}
 }
